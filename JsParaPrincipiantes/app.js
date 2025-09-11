cd43db453f02801ecba964f7c9b871877b7f6469
Cooky <tonygarcia692@gmail.com>
2025-09-10 18:20:55 -0600

    JsPrincipiantes

--- JsParaPrincipiantes/app.js
+++ JsParaPrincipiantes/app.js
@@ -1,3 +1,34 @@
 // Todos los archivos de java terminan en .js 
 
-document.write("<h1>Hola Mundo</h1>");
+document.write("<h1>Hola Mundo");
+
+//--------------------------------//
+
+console.log("Esto es una cadena de texto");
+
+//--------------------------------//
+
+// Tipos de datos
+"hola mundo"; // String
+'hola mundo'; // String
+`hola mundo`; // String (template literal)
+
+123; // Number
+
+true; // Boolean
+false; // Boolean
+null; // Null
+undefined; // Undefined
+
+//array
+[1, 2, 3];
+["Joe", "Ryan", "John"];
+[true, false, true];
+
+//objeto
+{ 
+    "nombre": 'Joe',
+    "edad": 30, 
+    "casado": true
+    "Amigos": ["Ryan", "John"] 
+};
