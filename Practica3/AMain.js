4e8f15fe9fe3473a663f51d78afda2090f1cd432
Cooky <tonygarcia692@gmail.com>
2025-09-25 15:24:47 -0600

    Practica 3A
    Aqui usamos dos archivos para export e import

---
+++ Practica3/AMain.js
@@ -0,0 +1,4 @@
+import { restar } from './AUtils.js';
+console.log(restar(10, 4));
+console.log(restar(20, 5));
+console.log(restar(15, 7));
---
+++ Practica3/AUtils.js
@@ -0,0 +1,4 @@
+ export function restar(a, b) {
+    return a - b;
+}
+console.log(restar(5, 3)); 
